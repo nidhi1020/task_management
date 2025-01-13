587f079698c054816ab8f196351d9317d6c1fd06
Nidhi Biswas <nidhibiswas111@gmail.com>
2025-01-13 10:46:29 +0600

    Dynamic Url Implemented

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from tasks.views import home,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",home),
-    path("contact/",contact)
+    path("contact/",contact),
+    path("tasks/", include("tasks.urls")),
     
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
+
     #work with database
     #Transform data
     #Data pass
     #http/json response
+    
+def home(request):
     return HttpResponse("Welcome to the task management system")
 
 def contact(request):
@@ -14,3 +16,8 @@ def contact(request):
 
 def show_task(request):
     return HttpResponse("This is our task page")
+
+def show_specific_task(request, id):
+    print("id", id)
+    print("id type", type(id))
+    return HttpResponse(f"This is specific task page {id} ")
